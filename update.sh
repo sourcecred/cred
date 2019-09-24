cf2fa578a75acd98e871df86cab0c3c5f2996e5b
Dandelion Mané <decentralion@dandelion.io>
2019-09-24 16:07:44 +0200

    Update now saves the scores too
    It's kind of hacky, but should work.

--- update.sh
+++ update.sh
@@ -5,9 +5,10 @@ set -eu
 rm -rf ./docs
 PATH_TO_THIS_REPOSITORY="$(pwd)"
 cd $sc
-./scripts/build_static_site.sh \
-  --target $PATH_TO_THIS_REPOSITORY/docs  \
-  --project-file $PATH_TO_THIS_REPOSITORY/projects/combined.json \
-  --project-file $PATH_TO_THIS_REPOSITORY/projects/discourse.json \
-  --project-file $PATH_TO_THIS_REPOSITORY/projects/github.json \
-  --weights $PATH_TO_THIS_REPOSITORY/weights.json
+# ./scripts/build_static_site.sh \
+  # --target "$PATH_TO_THIS_REPOSITORY/docs"  \
+  # --project-file "$PATH_TO_THIS_REPOSITORY/projects/combined.json"
+  # --weights "$PATH_TO_THIS_REPOSITORY/weights.json"
+# Load it twice so we can access the scores -- it's a hack, pending real instance system
+node ./bin/sourcecred.js load --project "$PATH_TO_THIS_REPOSITORY/projects/combined.json" --weights "$PATH_TO_THIS_REPOSITORY/weights.json"
+node ./bin/sourcecred.js scores @sourcecred | jq '.' > "$PATH_TO_THIS_REPOSITORY/scores.json"
