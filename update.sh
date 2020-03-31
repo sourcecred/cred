e8c1a9ac775a5112856d8bb96b406eb08be64d9c
Robin van Boven <497556+Beanow@users.noreply.github.com>
2020-03-31 13:51:40 +0200

    Track only the "combined" project file (#22)
    As the other projects served more as an example and were out of date.

--- update.sh
+++ update.sh
@@ -7,9 +7,9 @@ PATH_TO_THIS_REPOSITORY="$(pwd)"
 cd $sc
 ./scripts/build_static_site.sh \
   --target "$PATH_TO_THIS_REPOSITORY/docs"  \
-  --project-file "$PATH_TO_THIS_REPOSITORY/projects/combined.json" \
+  --project-file "$PATH_TO_THIS_REPOSITORY/project.json" \
   --weights "$PATH_TO_THIS_REPOSITORY/weights.json"
 
 # Load it twice so we can access the scores -- it's a hack, pending real instance system
-node ./bin/sourcecred.js load --project "$PATH_TO_THIS_REPOSITORY/projects/combined.json" --weights "$PATH_TO_THIS_REPOSITORY/weights.json"
+node ./bin/sourcecred.js load --project "$PATH_TO_THIS_REPOSITORY/project.json" --weights "$PATH_TO_THIS_REPOSITORY/weights.json"
 node ./bin/sourcecred.js scores @sourcecred | jq '.' > "$PATH_TO_THIS_REPOSITORY/scores.json"
